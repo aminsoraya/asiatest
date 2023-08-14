c5d60a9c73a1c7d571de532458854f2878a1156a
aminsoraya <aminsoraya@gmail.com>
2023-08-14 14:57:34 +0330

    .

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,5 @@
 import axios from "axios";
 
-// const instance =axios.create({
-//   baseURL: "https://exam.pishgamanasia.com/webapi",
-// });
-
-// instance.interceptors.request.use((config) => {
-//   let token = localStorage.getItem("asia_token");
-
-//   if (token )
-//     config.data = {
-//       ...config.data,
-//       token,
-//     };
-
-//   return config;
-// });
-
-// export default instance;
 
 const InstanceEnhancer = ({ appendToken }: { appendToken?: boolean }) => {
   const instance = axios.create({

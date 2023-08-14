9f7f354e1d731c69bb0f1e648464e52e06d24c10
aminsoraya <aminsoraya@gmail.com>
2023-08-14 10:43:33 +0330

    modify login component apperance

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./index.scss";
 
 export default function Login() {
-    
-
-    return (
-        <>
-            <span>this is login page</span>
-        </>
-    )
+  return (
+    <main className="main">
+      <div className="login">
+        <div className="header">
+          <span>ورود</span>
+        </div>
+        <div className="body">
+          <div className="input_row">
+            <label>نام کاربری</label>
+            <input type="text" />
+          </div>
+          <div className="input_row">
+            <label>کلمه عبور</label>
+            <input type="text" />
+          </div>
+          <div className="input_row">
+            <button>ورود</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
